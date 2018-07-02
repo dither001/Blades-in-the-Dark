691c5c93a9457289433f942f9b1ddaec9709ab15
dither001 <32025687+dither001@users.noreply.github.com>
2018-07-02 11:28:52 -0600

    Initial Commit

---
+++ src/Dice.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class Dice {
+	// static fields
+	private static final Random RAND = new Random();
+
+	// static methods
+	public static int roll(int faces) {
+		return roll(1, faces);
+	}
+
+	public static int roll(int dice, int faces) {
+		int result = 0;
+
+		dice = (dice < 1) ? 1 : dice;
+		faces = (faces < 1) ? 1 : faces;
+
+		for (int i = 0; i < dice; ++i) {
+			result += RAND.nextInt(faces) + 1;
+		}
+
+		return result;
+	}
+
+	public static int advantage(int faces) {
+		int firstRoll = roll(1, faces);
+		int secondRoll = roll(1, faces);
+
+		return (firstRoll > secondRoll) ? firstRoll : secondRoll;
+	}
+
+	public static int disadvantage(int faces) {
+		int firstRoll = roll(1, faces);
+		int secondRoll = roll(1, faces);
+
+		return (firstRoll < secondRoll) ? firstRoll : secondRoll;
+	}
+
+	public static int[] fortune() {
+		return fortune(0, 6);
+	}
+
+	public static int[] fortune(int dice) {
+		return fortune(dice, 6);
+	}
+
+	public static int[] fortune(int dice, int faces) {
+		int[] fortune = new int[faces];
+
+		if (dice > 0) {
+			for (int i = 0; i < dice; ++i) {
+				++fortune[roll(faces) - 1];
+			}
+		} else {
+			++fortune[disadvantage(faces) - 1];
+		}
+
+		return fortune;
+	}
+
+	public static <T> T randomFromArray(T[] array) {
+		T choice = array[Dice.roll(array.length) - 1];
+
+		return choice;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T randomFromList(List<T> list) {
+		T[] array = (T[]) new Object[list.size()];
+		list.toArray(array);
+		T choice = array[Dice.roll(array.length) - 1];
+
+		return choice;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T randomFromSet(Set<T> set) {
+		T[] array = (T[]) new Object[set.size()];
+		set.toArray(array);
+		T choice = array[Dice.roll(array.length) - 1];
+
+		return choice;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> List<T> sublistFromList(int length, List<T> list) {
+		T[] array = (T[]) new Object[list.size()];
+		list.toArray(array);
+
+		List<T> sublist = new ArrayList<T>();
+		T choice;
+		while (sublist.size() < length) {
+			choice = array[Dice.roll(array.length) - 1];
+
+			if (sublist.contains(choice) != true)
+				sublist.add(choice);
+		}
+
+		return sublist;
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,96 @@
+import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+	private static int PCS_TO_ROLL = 50;
+	private static int SPELLBOOK_LEVEL = 17;
+
+	//
+	private static final Scanner INPUT = new Scanner(System.in);
+	private static int proceed = 1;
+
+	public static void main(String[] args) {
+		// TODO
+
+		workLoop();
+		// rollRogues();
+		// setupActorLadder();
+		// characterAdvance(Class.BARBARIAN);
+		// rollCharacter();
+
+	}
+
+	public static void printFactionStuff() {
+		List<Crew> list = Crew.getFactions();
+		for (Iterator<Crew> it = list.iterator(); it.hasNext();) {
+			System.out.println(it.next().npcAllyGet().toString());
+		}
+	}
+
+	public static void rollRogues() {
+		for (int i = 0; i < PCS_TO_ROLL; ++i) {
+			rollOneRogue();
+			System.out.println();
+		}
+	}
+
+	public static void rollOneRogue() {
+		Crew crew = new Crew();
+		Rogue rogue = new Rogue(crew);
+		System.out.println(rogue.toStringDetailed());
+	}
+
+	public static void rollCrew() {
+		Crew crew = new Crew();
+		System.out.println(crew.toString());
+	}
+
+	public static void workLoop() {
+		int score = 0;
+		Crew crew = new Crew();
+
+		String line;
+		while (proceed == 1) {
+			System.out.println(" - - - - - - - - score: " + ++score);
+			System.out.println(crew.toStringDetailed());
+			System.out.println();
+
+			// Crew target = Crew.getCrewByFaction(crew.preferredTarget());
+			// Score score = new Score(crew, target);
+			// System.out.println(score.toString());
+			//
+			// System.out.println();
+			// score.action();
+
+			crew.findWork();
+			crew.advance();
+
+			line = INPUT.nextLine();
+			try {
+				proceed = Integer.parseInt(line);
+			} catch (NumberFormatException e) {
+				proceed = 1;
+			}
+
+			if (proceed != 0) {
+				if (proceed == 2) {
+					List<Crew>[] array = crew.nonNeutralStatus();
+					System.out.println();
+					System.out.println("Allies: " + array[5].toString());
+					System.out.println("Friendlies: " + array[4].toString());
+					System.out.println("Helpful: " + array[3].toString());
+					System.out.println("Indifferent: " + array[2].toString());
+					System.out.println("Hostiles: " + array[1].toString());
+					System.out.println("Enemies: " + array[0].toString());
+					System.out.println();
+				}
+
+				proceed = 1;
+			}
+		}
+	}
+
+}
